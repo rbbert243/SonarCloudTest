9af07d1daba1c271f28f79bc0bcc41dcb1f7d94c
Robert-Iulian Balan <94171469+rbbbert@users.noreply.github.com>
2023-05-31 00:08:07 +0300

    Add project files.

---
+++ SonarCloudTest/SonarCloudTest.cpp
@@ -0,0 +1,15 @@
+﻿// SonarCloudTest.cpp : Defines the entry point for the application.
+//
+
+#include "SonarCloudTest.h"
+
+using namespace std;
+
+#define ERROR "error"
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	int error[12];
+	return 0;
+}
